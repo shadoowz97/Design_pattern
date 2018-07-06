fa5c822ae19259cb8a15d3367049d95be4f68c72
shadoowz97 <827122714@qq.com>
2018-07-06 16:55:08 +0800

    commit

--- Design_Pattern/src/adapter_pattern/class_adapter/PrintBanner.java
+++ Design_Pattern/src/adapter_pattern/class_adapter/PrintBanner.java
@@ -1,5 +1,21 @@
 package adapter_pattern.class_adapter;
-
+/*
+ * Adapter模式中登场的角色
+ * Target
+ * 该角色负责定义所需的方法即Print接口或者抽象类Print
+ * 
+ * Client
+ * 该角色负使用Target角色所定义的方法进行具体的处理
+ * Main类中正是调用Target方法的角色
+ * 
+ * Adaptee
+ * Adaptee是持有既定方法的角色，是被适配的角色，即Banner类，假如Target方法和adaptee中的方法完全相同，那么其实是不需要Adapter类的
+ * 
+ * Adapter
+ * 使用Adaptee已经实现的功能来完成Target方法的需求
+ * 
+ * 
+ * */
 public class PrintBanner extends Banner implements Print {
 	public PrintBanner(String string) {
 		super(string);
---
+++ Design_Pattern/src/adapter_pattern/exer/FileProperties.java
@@ -0,0 +1,38 @@
+package adapter_pattern.exer;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FileProperties implements FileIO{
+    Properties prop;
+    public FileProperties(){
+    	
+    }
+	@Override
+	public void readFromFile(String filename) throws IOException {
+		// TODO Auto-generated method stub
+		prop.load(new FileInputStream(filename));
+		
+	}
+
+	@Override
+	public void writeToFile(String filename) throws IOException {
+		// TODO Auto-generated method stub
+		prop.store(new FileOutputStream(filename), "#written by FileProperties\r\n#2018/07/06");
+	}
+
+	@Override
+	public void setValue(String key, String value) {
+		// TODO Auto-generated method stub
+		prop.setProperty(key, value);
+	}
+
+	@Override
+	public void getValue(String key) {
+		// TODO Auto-generated method stub
+		prop.getProperty(key);
+	}
+
+}
