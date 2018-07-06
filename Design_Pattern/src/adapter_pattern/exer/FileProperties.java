22791acf2c273ccfd5f597f275ff5eee23c47070
shadoowz97 <827122714@qq.com>
2018-07-06 17:06:48 +0800

    commit

--- Design_Pattern/src/adapter_pattern/exer/FileProperties.java
+++ Design_Pattern/src/adapter_pattern/exer/FileProperties.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class FileProperties implements FileIO{
     Properties prop;
     public FileProperties(){
-    	
+    	prop=new Properties();
     }
 	@Override
 	public void readFromFile(String filename) throws IOException {
