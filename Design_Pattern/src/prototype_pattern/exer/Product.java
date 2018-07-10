0322ab2662ec9fd2c204d3f5df83b680ec8d79ec
shadoowz97 <827122714@qq.com>
2018-07-10 10:39:23 +0800

    commit

--- Design_Pattern/src/prototype_pattern/exer/Product.java
+++ Design_Pattern/src/prototype_pattern/exer/Product.java
@@ -1,5 +1,16 @@
 package prototype_pattern.exer;
 
-public class Product {
-
+public abstract class Product implements Cloneable {
+  public abstract void use(String s);
+  
+  public final Product createClone(){
+	  Product p=null;
+	  try{
+		  p=(Product)clone();
+	  }
+	  catch(CloneNotSupportedException e){
+		  e.printStackTrace();
+	  }
+	  return p;
+  }
 }
